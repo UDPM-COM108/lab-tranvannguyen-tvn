800fb295e762d7faa980c0079b6f70b69a41a902
Tran Van Nguyen <tranavannguyen2022nt@gmail.com>
2025-11-08 14:09:25 +0700

    Nop bai lab 1

---
+++ Lab1/BTLab1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>// thư viện
+
+int main(){
+    //printf("Hello World");// xuat ra man hinh Hello World
+    printf("\nHello World");
+    int a,b;
+    char ten[10];
+    printf("Nhap a va b: ");
+    scanf("%d%d",&a,&b);
+    printf("Nhap vao ten toi: ");
+    scanf("%s",ten);
+    return 0;
+}
